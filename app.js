7eccb4dce9542078019539d9964c8769b1397a59
Mirko <desio.mirko@gmail.com>
2026-04-22 10:46:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -622,7 +622,9 @@ function initInstallPopup() {
     const iosShareSvg = `<svg viewBox="0 0 24 24" fill="none" stroke="#007AFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="width: 22px; vertical-align: bottom; margin: 0 4px;"><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path><polyline points="16 6 12 2 8 6"></polyline><line x1="12" y1="2" x2="12" y2="15"></line></svg>`;
 
     // Testo unico per la descrizione preso da Excel
-    let instructions = getVal('Popup_Descrizione', 'Aggiungi il menu alla schermata home.');
+    // Prende il testo e trasforma in grassetto automatico tutto ciò che è tra virgolette
+    let rawText = getVal('Popup_Descrizione', 'Aggiungi il menu alla schermata home.');
+    let instructions = rawText.replace(/"(.*?)"/g, '<b>"$1"</b>');
 
     popup.innerHTML = `
         <img src="${iconPath}" class="pwa-icon-box">
